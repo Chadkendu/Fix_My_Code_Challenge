f3eb44fb155ce4db0e463a88592e8ae3dfbe1335
Kendu <kenduajayi@gmail.com>
2023-08-26 02:38:57 -0700

    Update lists.h

--- 0x00-challenge/4-delete_dnodeint/lists.h
+++ 0x00-challenge/4-delete_dnodeint/lists.h
@@ -1,17 +1,16 @@
-fndef _LISTS_H_
+#ifndef _LISTS_H_
 #define _LISTS_H_
 
 #include <stddef.h>
 
 /**
  * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the prev node
+ * @next: points to the next node
  *
  * Description: doubly linked list node structure
  * for Holberton project
- * @n: integer
- * @prev: this points to the prev node
- * @next: this points to the next node
- *
  */
 typedef struct dlistint_s
 {
@@ -21,8 +20,8 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-void free_dlistint(dlistint_t *head);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+void free_dlistint(dlistint_t *head);
 
 #endif
